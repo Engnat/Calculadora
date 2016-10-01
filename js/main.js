5570d9993635be03975ff29ba9b9fb1a71bbe9d0
CJC9 <engnatruve@gmail.com>
2016-10-01 08:07:09 -0500

    Se creo carpeta css y stylesheet.css. Fabian

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ window.onload = function()
   let value;
   var number_one;
   var number_two;
-  var operationInput;
-  
+
+
   var sumar = function(n1, n2){
      result = parseFloat(n1) + parseFloat(n2);
     return result;
@@ -27,36 +27,43 @@ window.onload = function()
     return result;
   };
 
+  var mod = function(n1, n2){
+    result = parseFloat(n1) % parseFloat(n2);
+    return result;
+  };
+
   //Fields Validations
-  while(isNaN(number_one) === true){
-    number_one = prompt("Ingrese el primer numero");
-  }
-
-  while(isNaN(number_two) === true){
-    number_two = prompt("Ingrese el segundo numero");
-  }
-
-  //Get operation value
-  operationInput = prompt("Ingrese operacion a realizar");
-  var cond = Number.isNaN(operationInput);
-  if(cond){
-    operationInput  = param.toLowerCase();
-  }
-
-
-  if(operationInput === "suma" || operationInput === "mas" || operationInput === "+"){
-    value = sumar(number_one,number_two);
-  }else if(operationInput === "resta" || operationInput ==="menos" || operationInput ==="-"){
-    value =  restar(number_one,number_two);
-  }else if(operationInput === "multiplicar" || operationInput ==="por" || operationInput ==="*"){
-    value =  multiplicar(number_one,number_two);
-  }else if(operationInput === "dividir" || operationInput ==="/"){
-    value =  dividir(number_one,number_two);
-  }else{
-    value = "Error en la operacion";
-  }
+    while(isNaN(number_one) == true){
+      number_one = prompt("Ingrese el primer numero");
+    }
+    console.log("Numero 1: "+number_one)
+    while(isNaN(number_two) == true){
+      number_two = prompt("Ingrese el segundo numero");
+    }
+    console.log("Numero 2: "+number_two)
+    //Get operation value
+    var operation = prompt("Ingrese operacion a realizar");
+    var cond = Number.isNaN(operation);
+    if(cond){
+      var operation  = param.toLowerCase();
+    }
 
 
-  document.getElementById("result").innerHTML = value;
+    if(operation === "suma" || operation === "sumar" || operation === "mas" || operation === "+"){
+      value = sumar(number_one,number_two);
+    }else if(operation === "resta" || operation === "restar" ||operation ==="menos" || operation ==="-"){
+      value =  restar(number_one,number_two);
+    }else if(operation === "multiplicacion" || operation === "multiplicar" || operation ==="por" || operation ==="*"){
+      value =  multiplicar(number_one,number_two);
+    }else if(operation === "division" || operation === "dividir" || operation === "dividido" || operation ==="/"){
+      value =  dividir(number_one,number_two);
+    }else if(operation === "mod" || operation === "modular" || operation ==="%"){
+      value =  mod(number_one,number_two);
+    }else{
+      value = "Error en la operacion";
+    }
 
-};
+
+    document.getElementById("result").innerHTML = value;
+
+}
