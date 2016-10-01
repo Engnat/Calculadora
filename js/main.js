371d680916699d4260023b7a2cf8015969d460b3
Engie Ruge <engnatruve@gmail.com>
2016-09-30 22:40:35 -0500

    Creacion de condicionales. Fabian

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ window.onload = function()
   let value;
   var number_one;
   var number_two;
-
+  var operationInput;
+  
   var sumar = function(n1, n2){
      result = parseFloat(n1) + parseFloat(n2);
     return result;
@@ -25,4 +26,37 @@ window.onload = function()
     result = parseFloat(n1) / parseFloat(n2);
     return result;
   };
+
+  //Fields Validations
+  while(isNaN(number_one) === true){
+    number_one = prompt("Ingrese el primer numero");
+  }
+
+  while(isNaN(number_two) === true){
+    number_two = prompt("Ingrese el segundo numero");
+  }
+
+  //Get operation value
+  operationInput = prompt("Ingrese operacion a realizar");
+  var cond = Number.isNaN(operationInput);
+  if(cond){
+    operationInput  = param.toLowerCase();
+  }
+
+
+  if(operationInput === "suma" || operationInput === "mas" || operationInput === "+"){
+    value = sumar(number_one,number_two);
+  }else if(operationInput === "resta" || operationInput ==="menos" || operationInput ==="-"){
+    value =  restar(number_one,number_two);
+  }else if(operationInput === "multiplicar" || operationInput ==="por" || operationInput ==="*"){
+    value =  multiplicar(number_one,number_two);
+  }else if(operationInput === "dividir" || operationInput ==="/"){
+    value =  dividir(number_one,number_two);
+  }else{
+    value = "Error en la operacion";
+  }
+
+
+  document.getElementById("result").innerHTML = value;
+
 };
