af3714ce4dfa75794f0769ba56e3a42dcadbfc73
Engie Ruge <engnatruve@gmail.com>
2016-09-30 20:40:50 -0500

    Inicio de ejercicio calculadora con javascript

---
+++ js/calculadora.js
@@ -0,0 +1,9 @@
+var calcular = function(num1,num2){
+  var numero1 = parseInt(prompt('Ingrese un número',""));
+  var numero2 = parseInt(prompt('Ingrese un segundo número',""));
+  var operaciones = prompt("Ingrese la Operación que desea realizar","");
+  var resultado = 0;
+  if(numero1  == Number ){
+
+  }
+};
