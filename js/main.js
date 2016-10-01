71c874a1d60b8abd59432c69ebe505b383e7e7b6
CJC9 <engnatruve@gmail.com>
2016-10-01 08:28:55 -0500

    Correccion de errores de sintaxis

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = function()
   let value;
   var number_one;
   var number_two;
-
+  var operationInput;
 
   var sumar = function(n1, n2){
      result = parseFloat(n1) + parseFloat(n2);
@@ -33,31 +33,31 @@ window.onload = function()
   };
 
   //Fields Validations
-    while(isNaN(number_one) == true){
+    while(isNaN(number_one) === true){
       number_one = prompt("Ingrese el primer numero");
     }
-    console.log("Numero 1: "+number_one)
-    while(isNaN(number_two) == true){
+    console.log("Numero 1: " + number_one);
+    while(isNaN(number_two) === true){
       number_two = prompt("Ingrese el segundo numero");
     }
-    console.log("Numero 2: "+number_two)
+    console.log("Numero 2: "+number_two);
     //Get operation value
-    var operation = prompt("Ingrese operacion a realizar");
-    var cond = Number.isNaN(operation);
+    operationInput = prompt("Ingrese operacion a realizar");
+    var cond = Number.isNaN(operationInput);
     if(cond){
-      var operation  = param.toLowerCase();
+      operationInput  = param.toLowerCase();
     }
 
 
-    if(operation === "suma" || operation === "sumar" || operation === "mas" || operation === "+"){
+    if(operationInput === "suma" || operationInput === "sumar" || operationInput === "mas" || operationInput === "+"){
       value = sumar(number_one,number_two);
-    }else if(operation === "resta" || operation === "restar" ||operation ==="menos" || operation ==="-"){
+    }else if(operationInput === "resta" || operationInput === "restar" ||operationInput ==="menos" || operationInput ==="-"){
       value =  restar(number_one,number_two);
-    }else if(operation === "multiplicacion" || operation === "multiplicar" || operation ==="por" || operation ==="*"){
+    }else if(operationInput === "multiplicacion" || operationInput === "multiplicar" || operationInput ==="por" || operationInput ==="*"){
       value =  multiplicar(number_one,number_two);
-    }else if(operation === "division" || operation === "dividir" || operation === "dividido" || operation ==="/"){
+    }else if(operationInput === "division" || operationInput === "dividir" || operationInput === "dividido" || operationInput ==="/"){
       value =  dividir(number_one,number_two);
-    }else if(operation === "mod" || operation === "modular" || operation ==="%"){
+    }else if(operationInput === "mod" || operationInput === "modular" || operationInput ==="%"){
       value =  mod(number_one,number_two);
     }else{
       value = "Error en la operacion";
@@ -66,4 +66,4 @@ window.onload = function()
 
     document.getElementById("result").innerHTML = value;
 
-}
+};
