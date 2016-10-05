87d0f3cf7c6fed499d0533b19237d520ae3d0919
CJC9 <engnatruve@gmail.com>
2016-10-04 22:01:44 -0500

    Ejercicio calculadora, funciones operator, number, clear

--- js/main2.js
+++ js/main2.js
@@ -1,32 +1,63 @@
 var calculator = {
-  op1 : null,
-  op2 : null,
+  result : null,
   operator : null,
-  check_input : function(){
+  check_input : function(){//validan que los valores del input
+    console.log("check Input");
+    var self = calculator;
     var input = document.getElementById("input");
-    var input_text = input.textContent;//Si hay contenido html en el, devuelve html como texto plano
+    var input_text = input.value;//Si hay contenido html en el, devuelve html como texto plano
     var errors = document.querySelector("#error");
     var input_number = Number(input_text);
-    if(input_number !== NaN){
-        if(this.op1 === null){
-          this.op1 = input_number;
-        }else {
-          this.op2 = input_number;
-        }
+    if(Number.isNaN(input_number)){
+        input.value = "";
     }
-  }
+  },
+  set_number: function(button){//obtenemos el valor del boton que seleccionemos
+    return function(){
+      var self = calculator;
+      var input = document.getElementById("input");
+      input.value = "" + input.value + button.textContent;//concatenamos los valores .agrega lo que tenia + lo que se esta oprimiendo
+    };//return
+  },//set_number
+  set_operator : function(button){
+    return function(){
+      var self = calculator;
+      if(self.result === null){
+
+      }
+      self.operator = button.textContent;
+    };
+  },//set_operator
+  clear: function(){
+    var self = calculator;
+    self.result = null;
+    self.operator=null;
+    var input = document.getElementById("input");
+    input.value = "";
+  },//clear
 };
 
 document.addEventListener("DOMContentLoaded", function(event){//agregar un escucha .lo soporta desde version js1
-
+  //una vez la página se haya recargado
   var output = document.getElementById("result");
   var controlpad = document.getElementById("controlpad");
-  var button_operators = document.querySelectorAll("#operators button");
-  var i = 0;
+  var button_operators = controlpad.querySelectorAll("#operators button");
+  var i;
   for(i =0; i<button_operators.length; i++){
-    button_operators[i].addEventListener("click", calculator.check_input);
+    button_operators[i].addEventListener("click", calculator.set_operator(button_operators[i]));
+  }
+  var button_numbers = controlpad.querySelectorAll("#numbers button");
+  var input = document.getElementById("input");
+  for(i=0 ; i<button_numbers.length; i++){
+    button_numbers[i].addEventListener(
+      "click" ,
+       calculator.set_number(button_numbers[i])
+     );//addEventListener*/
   }
 
+var button_clear = controlpad.querySelector("#clear");
+button_clear.addEventListener("click", calculator.clear);
+  console.log("Initiated");
 });
 
 
